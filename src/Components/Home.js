dc5e8f01aa5649a95398da3acc2a5950da4bffd0
Alexander Anand <77586218+aanand93@users.noreply.github.com>
2024-02-29 09:53:19 -0500

    fix home page

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Home = () => {
 	return (
 		<div>
-			<h1>Home Test</h1>
+			<h1>Find Remote Jobs</h1>
 			<h5>Welcome to my remote job search app. Click the jobs tab to begin!</h5>
 		</div>
 	);
